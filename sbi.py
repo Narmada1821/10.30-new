7f1602f848537d467ffd07952bf7be2dbfd07da0
Narmada1821 <nandurinarmada1821@gmail.com>
2025-03-16 16:39:53 +0530

    added git commands

--- sbi.py
+++ sbi.py
@@ -4,3 +4,11 @@ git commit -m “commit message” (files available in local repository)
 git log – it shows commit history
 git log --oneline – shows commit history in single line.
 git log --stat – it will show the statastics about the changes
+git rm filename – file name can be deleted
+git rm --cached filename – unstage the file from local repo
+git restore --staged filename – used for staging the file from working
+directory (if the file is unstaged by us)
+Git hub (central repo):
+git remote add origin githuburl
+git push origin branchname
+git pull origin branchname
